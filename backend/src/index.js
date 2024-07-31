818078de8e241a693bd41ceeffcf9b9fa5bcfeb6
kumaranup1234 <anupg2551@gmail.com>
2024-08-01 00:59:43 +0530

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const urlRoutes = require("./routes/url");
+const URL = require("./models/url");
+const { connectToMongoDB } = require("./connect")
+app.use(express.json());
+
+
+connectToMongoDB('mongodb+srv://admin:7635074651@cluster0.8rccap1.mongodb.net/short-url')
+    .then(() => console.log("MongoDB Connected"))
+
+app.use("/url", urlRoutes);
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId
+        },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now()
+                }
+            }
+        }
+    );
+    res.redirect(entry.redirectURL)
+})
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+});
