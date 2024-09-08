ebb920cc34dd14d732359a659f9f7ba916bf737b
kumaranup1234 <anupg2551@gmail.com>
2024-09-08 21:40:23 +0530

    moved index.js

--- backend/src/utils/generateQrCode.js
+++ backend/src/utils/generateQrCode.js
@@ -2,7 +2,7 @@ const QRCode = require('qrcode');
 
 async function generateQRCodeForUrl(shortUrl) {
     try {
-        // Generate QR code as a data URL (base64 string)
+        // Generate QR code as a data URL (base64 string
         const qrCodeDataUrl = await QRCode.toDataURL(shortUrl);
         return qrCodeDataUrl;
     } catch (err) {
