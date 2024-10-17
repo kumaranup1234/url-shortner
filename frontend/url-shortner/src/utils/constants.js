66f95dd5253ac4e2b3f86e965f45c23c00aa2446
kumaranup1234 <anupg2551@gmail.com>
2024-10-17 16:40:14 +0530

    Added new Base URl

--- frontend/url-shortner/src/utils/constants.js
+++ frontend/url-shortner/src/utils/constants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = 'https://url-shortner-yi5l.onrender.com'
+export const BASE_URL = 'https://trim-at.onrender.com'
 // http://localhost:3000
 // https://url-shortner-yi5l.onrender.com
+// https://trim-at.onrender.com
