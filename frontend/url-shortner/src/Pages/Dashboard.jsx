22be788655a8710ef0118211900a12afffd0a63d
kumaranup1234 <anupg2551@gmail.com>
2024-12-02 18:07:00 +0530

    changed some logic in the Dashboard.jsx

--- frontend/url-shortner/src/Pages/Dashboard.jsx
+++ frontend/url-shortner/src/Pages/Dashboard.jsx
@@ -11,6 +11,7 @@ import {Suspense, useEffect, useState} from "react";
 import toast from "react-hot-toast";
 import InfoCard from "../Cards/InfoCard.jsx";
 import WorldMap from "../Components/WorldMap.jsx";
+import TopPerformingLink from "../Cards/TopPerformingLink.jsx";
 
 const Dashboard = () => {
     const [data, setData] = useState({});
@@ -18,9 +19,11 @@ const Dashboard = () => {
     const getData = async () => {
         try {
             const response = await axiosInstance.get("api/users/getAll");
+            console.log(response.data);
             setData({
                 totalUrls : response.data.totalUrls,
                 totalClicksSum: response.data.totalClicksSum,
+                topUrl: response.data.topUrl
             })
         } catch (error) {
             toast.error(error.message);
@@ -32,18 +35,27 @@ const Dashboard = () => {
 
 
     return (
-        <div className="mb-16">
+        <div className="bg-gray-100 mb-16">
             <div>
-                <h4 className="p-4 ml-4 text-sm sm:text-base">* Overall analytics of the short url. For analytics of individual clicks of a url, please visit <Link to="/links" className="font-bold underline">Links</Link> and clicks the stats button.</h4>
+                <h4 className="p-4 ml-4 text-sm sm:text-base">* Overall analytics of the short url. For analytics of
+                    individual clicks of a url, please visit <Link to="/links"
+                                                                   className="font-bold underline">Links</Link> and
+                    clicks the stats button.</h4>
             </div>
 
             {/* InfoCards Section */}
             <div className="flex flex-wrap md:flex-nowrap space-y-6 md:space-y-0 md:space-x-6 p-2 ml-4 mr-4">
                 <div className="w-full md:w-1/2 lg:w-1/2 bg-white shadow-lg p-4 rounded-lg">
-                    <InfoCard heading={"Total short urls created"} info={data.totalUrls} />
+                    <InfoCard heading={"Total short urls created"} info={data.totalUrls}/>
                 </div>
                 <div className="w-full md:w-1/2 lg:w-1/2  bg-white shadow-lg p-4 rounded-lg">
-                    <InfoCard heading={"Total overall clicks from all Urls"} info={data.totalClicksSum} />
+                    <InfoCard heading={"Total overall clicks from all Urls"} info={data.totalClicksSum}/>
+                </div>
+            </div>
+
+            <div className="flex flex-wrap md:flex-nowrap space-y-6 md:space-y-0 md:space-x-6 p-2 ml-4 mr-4">
+                <div className="w-full md:w-1/2 lg:w-1/2 bg-white shadow-lg p-4 rounded-lg">
+                    <TopPerformingLink topUrl={data.topUrl}/>
                 </div>
             </div>
 
