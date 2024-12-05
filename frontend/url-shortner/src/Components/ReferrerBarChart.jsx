9971d18f10cafd4d4a820050ed35e38093f27d79
kumaranup1234 <anupg2551@gmail.com>
2024-12-05 23:54:50 +0530

    tweaking some ui for phones

--- frontend/url-shortner/src/Components/ReferrerBarChart.jsx
+++ frontend/url-shortner/src/Components/ReferrerBarChart.jsx
@@ -71,7 +71,7 @@ const ReferrerBarChart = ({ apiUrl }) => {
                     <ResponsiveContainer width="100%" height={360}>
                         <BarChart
                             data={referrerData}
-                            margin={{top: 10, right: 5, left: -20, bottom: 10}}
+                            margin={{top: 10, right: 5, left: -15, bottom: 10}}
                             barGap={5} // Reduce gap between bars
                             barSize={60} // Increase bar width
                         >
