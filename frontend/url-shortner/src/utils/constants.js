467379236006bad79cd5bdd8adbc18f00862c478
kumaranup1234 <anupg2551@gmail.com>
2024-12-11 14:59:26 +0530

    updated backend link

--- frontend/url-shortner/src/utils/constants.js
+++ frontend/url-shortner/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://trim-at.onrender.com'
+export const BASE_URL = 'https://t-ul.vercel.app'
 // http://localhost:3000
 // https://url-shortner-yi5l.onrender.com
 // https://trim-at.onrender.com
+// https://t-ul.vercel.app/
