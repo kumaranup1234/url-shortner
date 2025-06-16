a8913196578c530ae370337b56c3cceb9421fa4b
Anup Kumar <anupg2551@gmail.com>
2025-06-17 00:34:26 +0530

    changed url

--- frontend/url-shortner/src/utils/constants.js
+++ frontend/url-shortner/src/utils/constants.js
@@ -1,6 +1,7 @@
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://t-at.vercel.app'
 export const FRONTEND_BASE_URL = 'https://trimat.vercel.app'
 // http://localhost:3000
 // https://url-shortner-yi5l.onrender.com
 // https://trim-at.onrender.com
 // https://t-ul.vercel.app/
+// https://t-at.vercel.app/
